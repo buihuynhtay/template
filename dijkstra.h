496ccb07bf48a456b9a56022b5b5d9d059f361f7
buihuynhtay <139624524+buihuynhtay@users.noreply.github.com>
2023-12-05 21:47:21 +0700

    Add files via upload

---
+++ dijkstra.h
@@ -0,0 +1,20 @@
+void dijkstra(int s, vector<int> &minDist){
+    minDist.resize(n + 1, 1e18 + 7);
+    priority_queue<pll, vector<pll>, greater<pll>> pq;
+    minDist[s] = 0;
+    pq.push({s, minDist[s]});
+    while(pq.empty() == 0){
+        int u = pq.top().se;
+        ll dist = pq.top().fi;
+        pq.pop();
+
+        if(minDist[u] != dist) continue;
+        for(pii it : graph[u]){
+            int v = it.fi;
+            int w = it.se;
+            if(Min(minDist[v], minDist[u] + w)){
+                pq.push({minDist[v], v});
+            }
+        }
+    }
+}
